647305b7cf0db4d451a967685b700b7b1dd5dee9
FavyWorld <adeosunfavoradewole@gmail.com>
2023-11-23 09:01:49 +0100

    index.html

---
+++ assets/js/data_types.js
@@ -0,0 +1,4 @@
+let x=90
+let name="Favy";
+let is_alive=false;
+let is_old=true;
---
+++ assets/js/external_js.js
@@ -0,0 +1,9 @@
+console.log("Are you okay?");
+let x=1
+let age=34;
+let name="favor"
+let company="FavyWorld";
+console.log(x,age,name,company);
+const PI =3.142 
+// const can not be change because it is a constant
+
